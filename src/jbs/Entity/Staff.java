6fb2bdfe7dfae029f55edbb8e1a6dc2827d912c5
jack <ygglyang@gmail.com>
2018-09-03 18:07:20 +0800

    z：create staff entity！

--- src/jbs/Entity/Staff.java
+++ src/jbs/Entity/Staff.java
@@ -1,4 +1,31 @@
 package jbs.Entity;
 
 public class Staff {
+    private String sno;
+    private String userid;
+    private String sname;
+
+    public String getSno() {
+        return sno;
+    }
+
+    public void setSno(String sno) {
+        this.sno = sno;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
 }
